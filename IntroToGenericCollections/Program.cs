853f5a0d64ec853d670a6dea3ff3360fe6337064
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-09-24 13:58:07 -0400

    list ex

--- IntroToGenericCollections/Program.cs
+++ IntroToGenericCollections/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace IntroToGenericCollections
+﻿using System.Collections;
+
+namespace IntroToGenericCollections
 {
     internal class Program
     {
@@ -27,9 +29,53 @@
             }
 
             Console.WriteLine("Sum = " + sum);
-        }
 
 
 
+            // ArrayList
+
+            // PROS - dynamic in size
+            // CONS - boxing/unboxing - performance issues
+            // CONS - not type-safe
+
+            // WARNING - DO NOT USE IT
+
+            ArrayList myArrayList = new ArrayList();
+            myArrayList.Add(10);
+            myArrayList.Add(20);
+            myArrayList.Add(30);
+            myArrayList.Add(40);
+
+            //myArrayList.Add("hello");
+
+            sum = 0;
+
+            for (int i=0; i<myArrayList.Count; i++)
+            {
+                int num = (int)myArrayList[i];
+                sum += num;
+            }
+
+            Console.WriteLine("\nSum = " + sum);
+
+
+            // List
+
+            List<int> myList = new List<int>();
+            myList.Add(10);
+            myList.Add(20);
+            myList.Add(30);
+            myList.Add(40);
+
+            sum = 0;
+
+            for (int i=0; i<myList.Count; i++)
+            {
+                int num = myList[i];
+                sum += num;
+            }
+
+            Console.WriteLine("\nSum = " + sum);
+        }        
     }
 }
