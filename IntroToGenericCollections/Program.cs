d57e4f77a84c5f3abc655dc09ae77a9dad6faa4c
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-09-24 13:44:25 -0400

    array ex

---
+++ IntroToGenericCollections/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace IntroToGenericCollections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Array
+
+            // PROS - type-safe
+            // CONS - static in size
+
+            int[] myArray = new int[4];
+            myArray[0] = 10;
+            myArray[1] = 20;
+            myArray[2] = 30;
+            myArray[3] = 40;
+
+            //myArray[3] = "hello";
+            //myArray[4] = 50;
+
+            int sum = 0;
+
+            for (int i=0; i<myArray.Length; i++)
+            {
+                int num = myArray[i];
+                sum += num;
+            }
+
+            Console.WriteLine("Sum = " + sum);
+        }
+
+
+
+    }
+}
