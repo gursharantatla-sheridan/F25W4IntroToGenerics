127d64a6886488d02de8c477c56ddcf4297c1e62
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-09-26 13:25:17 -0400

    dict ex

--- DictionaryExample/Program.cs
+++ DictionaryExample/Program.cs
@@ -14,7 +14,23 @@ namespace DictionaryExample
             Employee emp2 = new Employee(102, "Anne", 7000);
             Employee emp3 = new Employee(103, "Mark", 3000);
 
+            Dictionary<int, Employee> dictEmps = new Dictionary<int, Employee>();
+            dictEmps.Add(emp1.Id, emp1);
+            dictEmps.Add(emp2.Id, emp2);
+            dictEmps.Add(emp3.Id, emp3);
 
+            //dictEmps.Add(emp1.Id, emp1);
+
+            Console.Write("Enter a key: ");
+            int key = Convert.ToInt32(Console.ReadLine());
+
+            if (dictEmps.ContainsKey(key))
+            {
+                Employee emp = dictEmps[key];
+                Console.WriteLine(emp);
+            }
+            else
+                Console.WriteLine("Invalid key, please try again");
         }
     }
 }
